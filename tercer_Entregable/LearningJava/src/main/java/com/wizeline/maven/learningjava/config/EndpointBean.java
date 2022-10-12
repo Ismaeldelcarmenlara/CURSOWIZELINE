eb6741d8015a5f5d98eeb75aed5bb57d7deb14e2
ISMAEL DEL CARMEN LARA <ismael.carmen@elektra.com.mx>
2022-10-12 01:21:44 -0500

    se integran pruebasunitarias

--- tercer_Entregable/LearningJava/src/main/java/com/wizeline/maven/learningjava/config/EndpointBean.java
+++ tercer_Entregable/LearningJava/src/main/java/com/wizeline/maven/learningjava/config/EndpointBean.java
@@ -4,6 +4,8 @@ import javax.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
+
+
 @Component
 @Validated
 @ConfigurationProperties(prefix = "consumers")
--- tercer_Entregable/LearningJava/src/test/java/com/wizeline/maven/learningjava/LearningjavamavenApplicationTests.java
+++ tercer_Entregable/LearningJava/src/test/java/com/wizeline/maven/learningjava/LearningjavamavenApplicationTests.java
@@ -1,13 +1,26 @@
 package com.wizeline.maven.learningjava;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.wizeline.maven.learningjava.service.UserServiceImpl;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class LearningjavamavenApplicationTests {
 
+	
+	@Autowired
+	private UserServiceImpl userServiceImpl;
+	
 	@Test
-	void contextLoads() {
+	void createUserTest() {
+		
+		userServiceImpl.createUser(null, null);
 	}
 
 }
